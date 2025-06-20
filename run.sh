6df21b9fd6be4686480cb3db2ea99d3f3ae79b3b
xuexcy <xuechengyun@gmail.com>
2025-06-20 14:28:55 +0800

    update

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+########################################################################
+#
+# Copyright (c) 2025 xx.com, Inc. All Rights Reserved
+#
+########################################################################
+# Author : xuechengyun
+# E-mail : xuechengyun@gmail.com
+# Date   : 2025/04/18 16:05:58
+# Desc   :
+########################################################################
+
+# set -x
+CUR_DIR=$(cd `dirname $0`; pwd)
+cd ${CUR_DIR}
+
+name=使用表达式模板做向量计算
+curl https://raw.githubusercontent.com/xuexcy/learning_more_cpp_idioms/refs/heads/main/src/expression_template_deps/README.md > ${CUR_DIR}/$name/index.md
+
+
+
+
+
